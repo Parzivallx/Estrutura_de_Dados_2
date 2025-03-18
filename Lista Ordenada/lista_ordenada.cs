609c3925422a266ec85bca146041297843d6999e
Parzivallx <emmanuel.gomes@amazoninf.com.br>
2025-03-18 11:02:16 -0300

    exemplos de código listas ordenadas

--- Lista Ordenada/lista_ordenada.cs
+++ Lista Ordenada/lista_ordenada.cs
@@ -1,1 +1,39 @@
+Listas ordenadas !!!
 
+
+// Figura 11 - Método de inserção em uma lista ordenada encadeada
+
+
+public void insereNoOrdenado(Node novo)
+{
+    Node aux;
+
+    // Verificando se a lista está vazia
+    if (primeiro == null || contaNos() == 0)
+    {
+        novo.Prox = primeiro;
+        primeiro = novo;
+    }
+    else
+    {
+        aux = primeiro;
+        int valorNovo = Convert.ToInt32(novo.Item); // Usar Item e converter corretamente
+        int valorAux = Convert.ToInt32(aux.Item);   // Usar Item e converter corretamente
+
+        // Lógica para encontrar o ponto de inserção
+        if (valorNovo < valorAux)
+        {
+            novo.Prox = primeiro;
+            primeiro = novo;
+        }
+        else
+        {
+            while (aux.Prox != null && Convert.ToInt32(aux.Prox.Item) < valorNovo)
+            {
+                aux = aux.Prox;
+            }
+            novo.Prox = aux.Prox;
+            aux.Prox = novo;
+        }
+    }
+}
